af27affacc4e06cc20944c5cb1041ce8360b6d13
paryaM <parya797@yahoo.com>
2024-08-04 11:33:15 -0400

    new

--- system-reporter/main.py
+++ system-reporter/main.py
@@ -1,10 +1,10 @@
 from flask import jsonify 
-from memory.db_handler import Database 
+from memory.db_handler import Database_memory
 from memory.services import RamStatsService 
 from memory.api import app 
 
 if __name__ == "__main__": 
-    db = Database("memory/ram_stats.db") 
+    db = Database_memory("memory/ram_stats.db") 
     db.initialize_db() 
     
     RamStatsService.record_ram_stats("memory/ram_stats.db") 
